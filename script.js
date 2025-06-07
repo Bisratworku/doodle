6d8f24265605f0a7438582acce6be6a0a73ff4cc
Bisratworku <101055036+Bisratworku@users.noreply.github.com>
2025-06-07 12:09:36 -0700

    made the javescript to draw into the canvas then send the data to flask server

---
+++ script.js
@@ -0,0 +1,58 @@
+//=nb
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext("2d");
+const btn = document.getElementById("btn");
+ctx.fillStyle = 'white';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+let drawColor = 'black';
+let drawWidth = "5";
+let isDrawing = false;
+
+canvas.addEventListener("touchstart", start, false);
+canvas.addEventListener("touchmove", draw, false);
+canvas.addEventListener("mousedown", start, false);
+canvas.addEventListener("mousemove", draw, false);
+canvas.addEventListener("touchend", () => isDrawing = false, false);
+canvas.addEventListener("mouseup", () => isDrawing = false, false);
+btn.addEventListener('click', save, false);
+
+function start(event) {
+    isDrawing = true;
+    let rect = canvas.getBoundingClientRect();
+    let x = event.clientX - rect.left;
+    let y = event.clientY - rect.top;
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    event.preventDefault();
+}
+
+function draw(event){
+     if(isDrawing){
+        let rect = canvas.getBoundingClientRect();
+        let x = event.clientX - rect.left;
+        let y = event.clientY - rect.top;
+        
+        ctx.lineTo(x, y);
+        ctx.strokeStyle = drawColor;
+        ctx.linewidth = drawWidth;
+        ctx.lineCap = 'round';
+        ctx.lineJoin = 'round';
+        ctx.stroke();
+     }    
+}
+
+function save(event) {
+    const data = ctx.getImageData(0,0,canvas.width, canvas.height);
+    const send = fetch('/api/data', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            width: canvas.width,
+            height: canvas.height,
+            data: Array.from(data.data)
+        })
+    })
+}
